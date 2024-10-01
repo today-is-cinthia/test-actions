929697f25f1623624b7516e4cd2d98d6a8f32256
Cinthia <87044276+today-is-cinthia@users.noreply.github.com>
2024-10-01 10:20:44 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import unittest 
+import os
+import pathlib
 
 # Configura opciones de Chrome
 chrome_options = Options()
